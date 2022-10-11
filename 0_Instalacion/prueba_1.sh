3cf2369316da3ebca63c40cf370d47f94c86e461
Federico Esteban <federico.esteban@gmail.com>
2022-10-11 13:13:34 -0300

    Add proxy info

--- 0_Instalacion/prueba_1.sh
+++ 0_Instalacion/prueba_1.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+
+# Proxy de la FCEN (direccion de proxy:numero de puerto).
+# Descomentar si se esta en FCEN
+export http_proxy="http://proxy.fcen.uba.ar:8080"
+
 # Probar prueba_1: Asegurar que la instalación de GMT es correcta.
 gmt begin prueba1 png
 	gmt grdimage @earth_relief_20m -R-85/-20/-56/15 -JM15c -I+d
 	gmt coast -W -N1 -B
-gmt end # show
+gmt end show

049d91b86de509ce2ce096a7a45d3220a47c9cff
Esteban82 <federico.esteban@gmail.com>
2022-10-18 09:38:14 -0300

    sismos

--- 2_NuevosEjercicios/Sismos/obtener_sismos_USGS.sh
+++ 2_NuevosEjercicios/Sismos/obtener_sismos_USGS.sh
@@ -6,26 +6,36 @@
 # Se pueden editar las fechas y magnitudes. 
 SITE="https://earthquake.usgs.gov/fdsnws/event/1/query.csv"
 TIME="starttime=2021-01-01%2000:00:00&endtime=2021-12-31%2000:00:00"
-#TiempoInicial=2021-01-01%2000:00:00
-#TiempoFinal=2021-12-31%2000:00:00
+#StartTime=2021-01-01%2000:00:00
+#EndTime=2021-12-31%2000:00:00
 #Region=
 
 MAG="minmagnitude=5"
 #Magnitud minima=
 ORDER="orderby=time-asc"
+minlongitude=-80
+maxlongitude=-50
+minlatitude=-70
+maxlatitude=-40
+
 # Se puede modificar??
 URL="${SITE}?${TIME}&${MAG}&${ORDER}"
+#URL="${SITE}?${TIME}&${MAG}&${ORDER}&"
+
+echo $URL
 
 # Descargar los datos y reformatearlos.
 #curl ${URL} > test.txt
 gmt convert ${URL} -i2,1,3,4,0 -hi1 > quakes.txt
 #gmt which ${URL} -G
 #gmt which @usgs_quakes_22.txt -G
-wc quakes.txt
+#wc quakes.txt
 
-gmt plot quakes.txt -Sc0.1c -W0.1 -Gred -Baf -Ra -png sismos -fg 
+#gmt plot quakes.txt -Sc0.1c -W0.1 -Gred -Baf -Ra -png sismos -fg 
 
-URL="https://www.ldeo.columbia.edu/~gcmt/projects/CMT/catalog/jan76_dec17.ndk"
-gmt which $URL -G
-gawk '/^PDE/ {Date=$2; Time=$3; Lat=$4; Long=$5; Depth=$6; getline; Name=$1; getline; getline; Exp=$1; getline; mant=$11; strike1=$12; dip1=$13; rake1=$14; strike2=$15; dip2=$16; rake2=$17; print Long, Lat, Depth, strike1, dip1, rake1, strike2, dip2, rake2, mant, Exp, Date "T" Time, Name}' jan76_dec17.ndk | sed 's/\//-/g' > meca.gmt
+
+# 2. Descargar Mecanismos focales y reformatearlos
+#URL="https://www.ldeo.columbia.edu/~gcmt/projects/CMT/catalog/jan76_dec20.ndk"
+#gmt which $URL -G
+#gawk '/^PDE/ {Date=$2; Time=$3; Lat=$4; Long=$5; Depth=$6; getline; Name=$1; getline; getline; Exp=$1; getline; mant=$11; strike1=$12; dip1=$13; rake1=$14; strike2=$15; dip2=$16; rake2=$17; print Long, Lat, Depth, strike1, dip1, rake1, strike2, dip2, rake2, mant, Exp, Date "T" Time, Name}' jan76_dec20.ndk | sed 's/\//-/g' > meca.gmt
 # gmt select meca.gmt -R-75.1/-63/-34.44/-30.35 > GCMT_1976-2017_meca.gmt

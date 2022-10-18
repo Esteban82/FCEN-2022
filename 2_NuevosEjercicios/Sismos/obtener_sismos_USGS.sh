e2fdb9239257761c385b25cc23a513d9a2b40ff8
Esteban82 <federico.esteban@gmail.com>
2022-10-18 08:49:33 -0300

    mecanimos focales

--- 2_NuevosEjercicios/Sismos/obtener_sismos_USGS.sh
+++ 2_NuevosEjercicios/Sismos/obtener_sismos_USGS.sh
@@ -27,3 +27,5 @@ gmt plot quakes.txt -Sc0.1c -W0.1 -Gred -Baf -Ra -png sismos -fg
 
 URL="https://www.ldeo.columbia.edu/~gcmt/projects/CMT/catalog/jan76_dec17.ndk"
 gmt which $URL -G
+gawk '/^PDE/ {Date=$2; Time=$3; Lat=$4; Long=$5; Depth=$6; getline; Name=$1; getline; getline; Exp=$1; getline; mant=$11; strike1=$12; dip1=$13; rake1=$14; strike2=$15; dip2=$16; rake2=$17; print Long, Lat, Depth, strike1, dip1, rake1, strike2, dip2, rake2, mant, Exp, Date "T" Time, Name}' jan76_dec17.ndk | sed 's/\//-/g' > meca.gmt
+# gmt select meca.gmt -R-75.1/-63/-34.44/-30.35 > GCMT_1976-2017_meca.gmt

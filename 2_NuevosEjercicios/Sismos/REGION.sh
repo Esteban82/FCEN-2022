30ee166580c995130f66f53b8b40beb325fb9da5
Federico Esteban <federico.esteban@gmail.com>
2022-10-31 15:42:27 -0300

    sismos

--- 2_NuevosEjercicios/Sismos/REGION.sh
+++ 2_NuevosEjercicios/Sismos/REGION.sh
@@ -4,11 +4,20 @@
 	REGION="-79/-20/-63/-20"
 
 #	Crear arregle	
-	IFS='/'; R=( $REGION )
+#	IFS='/'; R=( $REGION )
+#	echo "value = ${R[0]}"
+#	echo "value = ${R[1]}"
+#	echo "value = ${R[2]}"
+#	echo "value = ${R[3]}"
+#	echo -e '\a'
+
+# Ver TR para convertir el / a un separador estandar de ITF^
+ 
+ R=(echo "-79/-20/-63/-20" | tr '/' '\t')
+ cat $REGION | tr '/' '\t'
+	#IFS='/'; R=( $REGION )
 	echo "value = ${R[0]}"
 	echo "value = ${R[1]}"
 	echo "value = ${R[2]}"
 	echo "value = ${R[3]}"
 	echo -e '\a'
-
-# Ver TR para convertir el / a un separador estandar de ITF

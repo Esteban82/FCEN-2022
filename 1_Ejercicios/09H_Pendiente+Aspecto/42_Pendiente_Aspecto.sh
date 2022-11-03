e55e2a6e69374da79c8b41a50855022e3c1a8dd1
Esteban82-HP <SU-CORREO-ELECTRONICO0>
2022-11-02 23:18:23 -0300

    42

--- 1_Ejercicios/09H_Pendiente+Aspecto/42_Pendiente_Aspecto.sh
+++ 1_Ejercicios/09H_Pendiente+Aspecto/42_Pendiente_Aspecto.sh
@@ -14,9 +14,14 @@
 	PROJ=M15c
 	REGION=-72/-64/-35/-30
 #	REGION=-68.5/-67.5/-32/-31
+
 	
 #	Grilla	
-	DEM=@earth_relief_30s
+	DEM=@earth_relief_30s_p
+
+#	CPT creadas ad-hoc
+#	CPT=Aspect-Slope_Brewer
+	CPT=Aspect-Slope_Esteban
 
 # 	Nombre archivo de salida
 	CUT=tmp_$title.nc
@@ -30,19 +35,22 @@ gmt begin $title png
 #	Setear la region y proyeccion
 	gmt basemap -R$REGION -J$PROJ -B+n
 
-#	Calcular Grillas con modulo del pendiente y aspecto
-	gmt grdgradient $DEM -D  -S$CUT  -R$REGION -fg
-	gmt grdgradient $DEM -Da -G$CUT2 -R$REGION -fg
+#	Calcular Grillas con modulo del pendiente (-D -S) y aspecto (-Da)
+	gmt grdgradient $DEM -R$REGION -fg -D  -S$CUT
+	gmt grdgradient $DEM -R$REGION -fg -Da -G$CUT2
 
-#	Convertir modulo del gradiente a inclinacion (pendiente) en radianes (ATAN), y luego a grados (R2D)
-	gmt grdmath $CUT ATAN R2D = $CUT
+#	Convertir modulo del gradiente a inclinacion (pendiente) en grados (ATAND).
+	gmt grdmath $CUT ATAND = $CUT
 
-#	Reclasificar grillas de pendiente en 4 clases 
-#	gmt grdclip $CUT -G$CUT -Sb3/10 -Si3/12/20 -Si12/22/30 -Sa22/40
-#	gmt grdclip $CUT -G$CUT -Sb1/10 -Si1/10/20 -Si10/20/30 -Sa20/40
-	gmt grdclip $CUT -G$CUT -Sb1/10 -Si1/5/20  -Si5/10/30  -Sa10/40 
+#	Reclasificar grilla de pendiente en 4 clases.
+# 	Pendiente muy baja (menor a A), baja (entre A y B), moderada (entre B y C) y alta (mayor a C).
+	A=3 ; B=12 ; C=22	# Valores originales definidos por Bewer (1993)	
+#	A=1 ; B=10 ; C=20
+#	A=1 ; B=05 ; C=10
+	gmt grdclip $CUT -G$CUT -Sb$A/10 -Si$A/$B/20 -Si$B/$C/30 -Sa$C/40
 
-#	Reclasificar grilla de Aspecto en 8 clases (rangos de 45º en cada direccion N, NE, E, SE, S, SO, O, NO)
+#	Reclasificar grilla de Aspecto en 8 clases 
+#	En rangos de 45º en cada direccion (N, NE, E, SE, S, SO, O, NO)
 	gmt grdclip $CUT2 -G$CUT2 -Sb22.5/1 -Si22.5/67.5/2 -Si67.5/112.5/3 -Si112.5/157.5/4 -Si157.5/202.5/5 \
 	-Si202.5/247.5/6 -Si247.5/292.5/7 -Si292.5/337.5/8 -Sa337.5/1
 
@@ -51,8 +59,7 @@ gmt begin $title png
 	gmt grdmath $CUT $CUT2 ADD = $CUT
 
 #	Crear Imagen
-#	gmt grdimage $CUT -CAspect-Slope_Brewer.cpt
-	gmt grdimage $CUT -CAspect-Slope_Esteban.cpt
+	gmt grdimage $CUT -C$CPT.cpt
 
 #	-----------------------------------------------------------------------------------------------------------
 #	Dibujar frame
@@ -66,11 +73,15 @@ gmt begin $title png
 	gmt coast -N1/thin -N2/dashed
 
 #	Escala de colores
-#	gmt colorbar -DJRM+w11c+o0.5/0c -CAspect-Slope_Esteban.cpt -L0.1
-	gmt image CPT_Esteban.png -DjBRM+w3c+o0.2/0.2c -F+p+gwhite
+#	gmt colorbar -DJRM+w11c+o0.5/0c -C -L0.1
+	gmt image $CPT.png -DjBRM+w3c+o0.2/0.2c -F+p+gwhite
 
 #	-----------------------------------------------------------------------------------------------------------
 #	Cerrar el archivo de salida (ps)
 	gmt end
 
 	rm tmp_*
+
+#	Ejercicios sugeridos
+#	1. Probar otras combinaciones para reclasificar las pendientes (lineas 47 a 49).
+#	2. Definir nuevos valores para reclasificar las pendientes.

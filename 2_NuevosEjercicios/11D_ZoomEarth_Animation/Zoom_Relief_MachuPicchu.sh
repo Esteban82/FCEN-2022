8dcef4106d69421bfaf066e125ce9d0224a3f6a1
Federico Esteban <federico.esteban@gmail.com>
2022-11-08 13:37:30 -0300

    zoom_earth

--- 2_NuevosEjercicios/11D_ZoomEarth_Animation/Zoom_Relief_MachuPicchu.sh
+++ 2_NuevosEjercicios/11D_ZoomEarth_Animation/Zoom_Relief_MachuPicchu.sh
@@ -20,9 +20,6 @@ cat << EOF > main.sh
 gmt begin
 	gmt grdimage @earth_relief -Rd -JG$lon/$lat/14.9c+z\${MOVIE_COL0}+v60 -Yc -Xc -B0 -Ctopo.cpt -I+d --GMT_GRAPHICS_DPU=${dpc}c --MAP_FRAME_PEN=faint
 	echo $lon $lat | gmt plot -SE-10 -Gred@50
-#	L=\$(gmt grdcut @earth_relief -Rd -JG$lon/$lat/14.9c+z\${MOVIE_COL0}+v60 -D+t --GMT_GRAPHICS_DPU=${dpc}c)
-#	echo \${L} | gmt text -F+cTR+jTR+f8p
-	echo "altitude = \${MOVIE_COL0} L = \${L}"
 gmt end
 EOF
 gmt movie main.sh -Sbpre.sh -N$title -Tz -C15cx15cx100 -D36 -H8 -M0,png \

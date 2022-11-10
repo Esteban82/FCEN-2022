989247e1b30f3f5a9af52922313a9031eaa554dd
Esteban82-HP <SU-CORREO-ELECTRONICO0>
2022-11-09 23:37:03 -0300

    magick

--- 2_NuevosEjercicios/fotos/fotos1.sh
+++ 2_NuevosEjercicios/fotos/fotos1.sh
@@ -8,7 +8,7 @@
 
 
 # Pegar en horizontal
-#convert fotos/Snap-400.jpg fotos/Snap-401.jpg +append hz.jpg
+#ma
 
 # Pegar en vertical
 #convert fotos/Snap-400.jpg fotos/Snap-401.jpg -append vt.jpg
@@ -20,9 +20,9 @@ gm montage -geometry +12+12 -tile 1x7 fotos/*.jpg -bordercolor red gmMontage2.pn
 
 # Usar Imagemagick
 
-montage -geometry +12+12 -tile 4x2 fotos/*.jpg montage.jpg
+magick montage -geometry +12+12 -tile 4x2 fotos/*.jpg montage.jpg
 
 #montage 2008*.jpg -thumbnail 300x300 -set caption %t -bordercolor #E6E6FA  -background grey40 -pointsize 9 -density 144x144 +polaroid -resize 50%  -background white -geometry +1+1 -tile 4x4 -title "Las Vegas Travel 2008" polaroid_t.jpg
-#montage fotos/*.jpg -thumbnail 300x300 -set caption %t -background grey40 -pointsize 9 -density 144x144 +polaroid -resize 50%  -background white -geometry +1+1 -tile 4x4 -title "Las fotos de mi tesis" polaroid_t.jpg
+magick montage fotos/*.jpg -thumbnail 300x300 -set caption %t -background grey40 -pointsize 9 -density 144x144 +polaroid -resize 50%  -background white -geometry +1+1 -tile 4x4 -title "Las fotos de mi tesis" polaroid_t.jpg
 
 # Fuente: http://dptnt.com/2008/12/create-photo-montage-with-imagemagick/

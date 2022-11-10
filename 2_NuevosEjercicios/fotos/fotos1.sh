176db2d6f99556153d6cb76b889d18220b02d6f9
Esteban82 <federico.esteban@gmail.com>
2022-11-09 23:17:47 -0300

    montage

--- 2_NuevosEjercicios/fotos/fotos1.sh
+++ 2_NuevosEjercicios/fotos/fotos1.sh
@@ -14,6 +14,15 @@
 #convert fotos/Snap-400.jpg fotos/Snap-401.jpg -append vt.jpg
 
 # Hacer mosaico
-gm montage -geometry 640x480  -tile 2x3 fotos/*.jpg Montage.jpg
-gm montage -geometry 640x480 -tile 2x3 fotos/*.jpg -bordercolor red Montage2.png
+gm montage -geometry +12+12 -tile 2x4 fotos/*.jpg gmMontage.jpg
+gm montage -geometry +12+12 -tile 1x7 fotos/*.jpg -bordercolor red gmMontage2.png
+#gm montage -geometry 640x480 -tile 2x3 fotos/*.jpg -bordercolor red gmMontage2.png
 
+# Usar Imagemagick
+
+montage -geometry +12+12 -tile 4x2 fotos/*.jpg montage.jpg
+
+#montage 2008*.jpg -thumbnail 300x300 -set caption %t -bordercolor #E6E6FA  -background grey40 -pointsize 9 -density 144x144 +polaroid -resize 50%  -background white -geometry +1+1 -tile 4x4 -title "Las Vegas Travel 2008" polaroid_t.jpg
+#montage fotos/*.jpg -thumbnail 300x300 -set caption %t -background grey40 -pointsize 9 -density 144x144 +polaroid -resize 50%  -background white -geometry +1+1 -tile 4x4 -title "Las fotos de mi tesis" polaroid_t.jpg
+
+# Fuente: http://dptnt.com/2008/12/create-photo-montage-with-imagemagick/
--- 2_NuevosEjercicios/fotos/psimage/bitimage.sh
+++ 2_NuevosEjercicios/fotos/psimage/bitimage.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 #
 
-ps=bitimage.ps
+gmt begin bitimage2 png
 
 # Raw, plus just change fore or background
-gmt psimage @vader1.png -P -Dx0/0+w2i -F+pfaint -K > $ps
-gmt psimage @vader1.png -Dx2.25i/0+w2i -F+pfaint -Gred+b -O -K >> $ps
-gmt psimage @vader1.png -Dx4.5i/0+w2i -F+pfaint -Gred+f -O -K >> $ps
+    gmt image @vader1.png -Dx0/0+w2i     -F+pfaint
+    gmt image @vader1.png -Dx2.25i/0+w2i -F+pfaint -Gred+b
+    gmt image @vader1.png -Dx4.5i/0+w2i  -F+pfaint -Gred+f
 
 # Change both,then set transparent back or foreground
 # The middle will show all white in PS but white and transparent in PNG
-gmt psimage @vader1.png -Dx0i/2i+w2i -F+pfaint -Gred+f -Gyellow+b -O -K >> $ps
-gmt psimage @vader1.png -Dx2.25i/2i+w2i -F+pfaint -G+b -O -K >> $ps
-gmt psimage @vader1.png -Dx4.5i/2i+w2i -F+pfaint -G+f -O -K >> $ps
+    gmt image @vader1.png -Dx0i/2i+w2i    -F+pfaint -Gred+f -Gyellow+b
+    gmt image @vader1.png -Dx2.25i/2i+w2i -F+pfaint -G+b
+    gmt image @vader1.png -Dx4.5i/2i+w2i  -F+pfaint -G+f
 
 # Finally, mix transparency and change of color
-gmt psimage @vader1.png -Dx0i/4i+w2i -F+pfaint -G+f -Gyellow+b -O -K >> $ps
-gmt psimage @vader1.png -Dx2.25i/4i+w2i -F+pfaint -G+b -Gred+f -O -K >> $ps
-gmt psimage @vader1.png -Dx4.5i/4i+w2i -F+pfaint -Gwhite+b -Gblack+f -O >> $ps
+    gmt image @vader1.png -Dx0i/4i+w2i    -F+pfaint -G+f      -Gyellow+b
+    gmt image @vader1.png -Dx2.25i/4i+w2i -F+pfaint -G+b      -Gred+f
+    gmt image @vader1.png -Dx4.5i/4i+w2i  -F+pfaint -Gwhite+b -Gblack+f
+gmt end #show
+

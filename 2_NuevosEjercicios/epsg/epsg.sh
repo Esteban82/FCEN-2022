69d602175a578461451aa5327fd47bcb95cfef0a
Esteban82-HP <SU-CORREO-ELECTRONICO0>
2022-11-12 17:54:04 -0300

    bug

--- 2_NuevosEjercicios/epsg/epsg.sh
+++ 2_NuevosEjercicios/epsg/epsg.sh
@@ -1,12 +1,22 @@
-PROJ=EPSG:3857
+# Definir proyeccion segun codigo EPSG
+EPSG=4326       # WGS84, LONG LAT
+#EPSG=3857      # Web Mercator projection 
 #EPSG_32723
+EPSG=32720      # UTM 20S, WGS84
+#EPSG=53004      # Mercator
+#PROJ=M
 #PROJ=EPSG:49964
 #PROJ=EPSG:49910
 
-gmt begin mapa png
-    gmt set PROJ_ELLIPSOID mars
-
-   # gmt basemap 
-    gmt coast -R-180/180/-80/80 -J$PROJ -Baf -W1p
+gmt begin $EPSG png
+    #gmt set PROJ_ELLIPSOID mars
+    gmt basemap -RAR -JEPSG:$EPSG/15c -Baf
+    gmt basemap -RAR -JU-20/15c -Baf
+    #gmt coast -RAR -JEPSG:$EPSG/15c -Baf -W1.0p,red    -N1/1.0p,red
+    #gmt coast -RAR -J$PROJ/15c      -Baf -W0.1p,yellow -N1/0.2p,blue #-Xw
 
 gmt end #show
+
+# Mas info:
+# https://en.wikipedia.org/wiki/EPSG_Geodetic_Parameter_Dataset
+# https://epsg.io/

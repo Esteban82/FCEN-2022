3cb60bff97954c8aeec3ecd9bdf4c64d203239c7
Esteban82-HP <SU-CORREO-ELECTRONICO0>
2022-11-12 18:11:41 -0300

    bug2

--- 2_NuevosEjercicios/epsg/epsg_bug.sh
+++ 2_NuevosEjercicios/epsg/epsg_bug.sh
@@ -1,9 +1,10 @@
 # Definir proyeccion segun codigo EPSG
 EPSG=4326       # WGS84, LONG LAT
 EPSG=32720      # UTM 20S, WGS84
+gmt begin bug png
+#    gmt basemap -Bafg -JU-20/15c        -R-72/-60/-85/-20
+#    gmt basemap -Bafg -JEPSG:$EPSG/15c  --MAP_GRID_PEN=red
 
-
-gmt begin $EPSG png
-    gmt basemap -RAR -JEPSG:$EPSG/15c -Bafg
-    gmt basemap -RAR -JU-20/15c -Bafg
+    gmt basemap -Bafg -JU-20/15c        -RAR+r1    -Xw
+    gmt basemap -Bafg -JEPSG:$EPSG/15c  --MAP_GRID_PEN=red
 gmt end

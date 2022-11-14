f4e384ae4d4d7d256a49d12d31c8d52b2f63e47d
Esteban82 <federico.esteban@gmail.com>
2022-11-14 11:47:32 -0300

    grep3

--- 2_NuevosEjercicios/2_grep/grep.sh
+++ 2_NuevosEjercicios/2_grep/grep.sh
@@ -4,43 +4,55 @@
 
 # Archivo de prueba
 # ---------------------------------------------
-# Archivo txt (campos separado espacios)
-file1=../Sismos/Datos/quakes.txt
-
 # Archivo CSV (campos separado por comas)
-file2=../0_sed/query.csv
+file=../0_sed/query.csv
 # ---------------------------------------------
 
 # 1. GMT: Opcion -e
-# SOLO analiza registros. No tiene en cuenta encabezados de archivos y segmetnos. 
+# ---------------------------------------------------------
+# SOLO analiza registros (No tiene en cuenta encabezados de archivos y segmentos).
 # A. Leer archivo entero 
-gmt info $file2 -h1
+gmt info $file -h1
 
 # B. Leer solo registros que contegan el texto: Scotia
-gmt info $file2 -h1 -eScotia
+gmt info $file -h1 -eScotia
 
 # C. Leer los que NO contengan SCOTIA
-gmt info $file2 -h1 -e~Scotia
-
+gmt info $file -h1 -e~Scotia
 
 # 2. Una celda valor identico a (AWK ==)
-# A. Por ejemplo, buscar sismos con profundidad de 33 km 
-#awk -F"," '$4==33' $file1
+# ---------------------------------------------------------
+# A. Por ejemplo, buscar sismos con profundidad de 33 km (profundidad por defecto por la falta de datos)
+#awk -F"," '$4==33' $file
 
 # 3. GREP
-# Globally Search For Regular Expression and Print out 
+# ---------------------------------------------------------
+# Globally Search For Regular Expression and Print out (Búsqueda global de expresiones regulares)
 # Analiza el archivo completo (incluyendo encabezados)
-# (Búsqueda global de expresiones regulares
 
 # Sintaxis
 # grep '<texto-buscado>' <archivo/archivos>
 
+# A. Que el registro contenga el texto "Scotia"
+grep Scotia $file | gmt info
 
-# A. Que el registro contenga el texto> Islands
-grep Scotia $file2 | gmt info
+# B. Inversa (-v, --invert-match)
+grep Scotia $file -v | gmt info
 
-# ... y luego usar sort para ordenar
-#awk -F"," '$4>300 {print $4}' $file2 | sed '1d' | sort
+# C. -c (--count): Imprimir el número de líneas de coincidencias 
+grep "Tierra del Fuego" $file -c #| gmt info
+
+# D. -A (--after-context): - imprimir las líneas después del patrón coincidente
+
+# E. -B (--before-context) - imprimir las líneas antes del patrón coincidente
+
+# F. -C (--context): es igual a -A + -B.
+
+# Expresiones regulares para patrones
+# F. ^<texto-buscado> - Inicio de línea
+
+# G. <texto-buscado>$ - fin de la línea
+
 
 # Fuente 
 # https://www.freecodecamp.org/espanol/news/grep-command-tutorial-how-to-search-for-a-file-in-linux-and-unix-with-recursive-find/

6d93eaceb8b7cd8616d10b109453b6fac2e61ac0
Federico Esteban <federico.esteban@gmail.com>
2022-11-15 13:10:18 -0300

    epsg

--- 2_NuevosEjercicios/epsg/epsg.sh
+++ 2_NuevosEjercicios/epsg/epsg.sh
@@ -1,20 +1,12 @@
 # Definir proyeccion segun codigo EPSG
 EPSG=4326       # WGS84, LONG LAT
-#EPSG=3857      # Web Mercator projection 
-#EPSG_32723
-EPSG=32720      # UTM 20S, WGS84
-#EPSG=53004      # Mercator
-#PROJ=M
-#PROJ=EPSG:49964
-#PROJ=EPSG:49910
+EPSG=3857      # Web Mercator projection 
+#EPSG=32720      # UTM 20S, WGS84
+echo $EPSG
 
 gmt begin $EPSG png
-    #gmt set PROJ_ELLIPSOID mars
-    gmt basemap -RAR -JEPSG:$EPSG/15c -Baf
-    gmt basemap -RAR -JU-20/15c -Baf
-    #gmt coast -RAR -JEPSG:$EPSG/15c -Baf -W1.0p,red    -N1/1.0p,red
-    #gmt coast -RAR -J$PROJ/15c      -Baf -W0.1p,yellow -N1/0.2p,blue #-Xw
-
+    gmt basemap -RAR -Jepsg:$EPSG/15c -Baf -V
+    gmt coast -W -N1
 gmt end #show
 
 # Mas info:

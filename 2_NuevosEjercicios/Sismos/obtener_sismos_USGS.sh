836383bca8bd4494caeb85dc8a857451a6a661ee
Federico Esteban <federico.esteban@gmail.com>
2023-02-07 17:25:24 -0300

    USGS

--- 2_NuevosEjercicios/Sismos/obtener_sismos_USGS.sh
+++ 2_NuevosEjercicios/Sismos/obtener_sismos_USGS.sh
@@ -1,28 +1,33 @@
 #!/usr/bin/env bash
 
-# Script original elaborado originlamente por Paul Wessel.
+# Script original elaborado originlamente por Paul Wessel. Modificaco por F. Esteban. 
 # Este script descarga datos de sismicidad del NEIC del USGS (US Geological Service)
 
-#   Datos para los sismos
-#StartTime=2021-01-01%2000:00:00
-#EndTime=2021-12-31%2000:00:00
-
-MAG="minmagnitude=5.5"  # Magnitud minima de los sismos
+# Datos para los sismos
 # Datos a modificar:
-# Se pueden editar las fechas y magnitudes. 
-SITE="https://earthquake.usgs.gov/fdsnws/event/1/query.csv"
-TIME="starttime=2001-01-01%2000:00:00&endtime=2021-12-31%2000:00:00"
-ORDER="orderby=time-asc"
+
+# Fechas
+Start=2022-01-01%2000:00:00
+End=2022-12-31%2000:00:00
+TIME="starttime=$Start&endtime=$End"
+#TIME="starttime=2001-01-01%2000:00:00&endtime=2021-12-31%2000:00:00"
+echo $TIME
+# Magnitudes
+MAG="minmagnitude=5.5"  # Magnitud minima de los sismos
+
+# Region Geografica
 ymin="minlatitude=-80"
 ymax="maxlatitude=50"
 xmin="minlongitude=-150"
 xmax="maxlongitude=-10"
 #xmin="minlongitude=-70"
 #xmax="maxlongitude=-10"
-# Se puede modificar??
+
+# NO modificar
+SITE="https://earthquake.usgs.gov/fdsnws/event/1/query.csv"
+ORDER="orderby=time-asc"
 URL="${SITE}?${TIME}&${MAG}&${ORDER}&${xmin}&${xmax}&${ymin}&${ymax}"
 echo $URL > URL.txt
-#URL="https://earthquake.usgs.gov/fdsnws/event/1/query.csv?starttime=2021-01-01%2000:00:00&endtime=2021-12-31%2000:00:00&minmagnitude=5&orderby=time-asc"
 #echo $URL >> URL.txt
 
 # Descargar los datos y reformatearlos.

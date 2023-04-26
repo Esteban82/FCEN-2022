8661b9078c5ccbafc99825cd59bee2cf1d9ec221
Esteban82 <federico.esteban@gmail.com>
2023-04-26 14:12:53 -0300

    actualizado

--- 2_NuevosEjercicios/fotos/gm_rotate.sh
+++ 2_NuevosEjercicios/fotos/gm_rotate.sh
@@ -1,10 +1,10 @@
 # Usar graphicsmagick
 # ---------------------------------------------------------------------
 # Rotar imagen 
-# Sintaxis basica
+# Sintaxis basica para un unico archivo
 #gm convert file.png -rotate 90 file_rot90.png
 
-# L
-for f in *.jpg; do
-    gm convert $f -rotate 90 rot_90_${f}
+# Rotar todos los archivos con extension jpg en una carpeta
+for imagen in *.jpg; do
+    gm convert $imagen -rotate 90 rot_90_${imagen}
 done;

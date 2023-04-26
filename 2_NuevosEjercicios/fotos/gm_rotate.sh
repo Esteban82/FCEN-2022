eb8802ad22abfbf03523ad4b0b19e1972e8ad114
Esteban82 <federico.esteban@gmail.com>
2023-04-26 14:13:41 -0300

    aclarar nombre de salida

--- 2_NuevosEjercicios/fotos/gm_rotate.sh
+++ 2_NuevosEjercicios/fotos/gm_rotate.sh
@@ -5,6 +5,7 @@
 #gm convert file.png -rotate 90 file_rot90.png
 
 # Rotar todos los archivos con extension jpg en una carpeta
+# El archivo de salida tienen el prefijo "rot_90_"
 for imagen in *.jpg; do
     gm convert $imagen -rotate 90 rot_90_${imagen}
 done;

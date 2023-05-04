5128188b885cf05de7fee8c49ca27a1aa38d299a
Esteban82 <federico.esteban@gmail.com>
2023-05-04 13:53:04 -0300

    Crop Image

---
+++ 2_NuevosEjercicios/fotos/gm_crop_image.sh
@@ -0,0 +1,5 @@
+# Usar graphicsmagick
+# ---------------------------------------------------------------------
+# Cortar imagen 
+# Sintaxis basica para un unico archivo
+gm convert magick.png -rotate 90 file_rot90.png

6b617ed1dd38fcbf07a8ad8f195b1bc3722c358f
Esteban82-HP <SU-CORREO-ELECTRONICO0>
2023-05-05 08:46:48 -0300

    crop image

--- 2_NuevosEjercicios/fotos/gm_crop_image.sh
+++ 2_NuevosEjercicios/fotos/gm_crop_image.sh
@@ -1,5 +1,12 @@
 # Usar graphicsmagick
 # ---------------------------------------------------------------------
+foto=magick.png
+
+# Obtener dimensiones
+file $foto
+gm identify $foto 
+gm identify -format '%w %h' $foto
+
 # Cortar imagen 
 # Sintaxis basica para un unico archivo
-gm convert magick.png -rotate 90 file_rot90.png
+gm convert $foto -crop 1000x1000+0+0 crop.png

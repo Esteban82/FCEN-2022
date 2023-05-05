29c0f2220e2b87d1d44b14c0a04e8ff138828741
Esteban82-HP <SU-CORREO-ELECTRONICO0>
2023-05-05 11:04:29 -0300

    crop image

--- 2_NuevosEjercicios/fotos/gm_crop_image.sh
+++ 2_NuevosEjercicios/fotos/gm_crop_image.sh
@@ -9,7 +9,7 @@ foto=magick.png
 
 # Sintaxis basica para un unico archivo
 #gm convert $foto -crop 1000x1000+0+0 crop.png  # width x height + originX + originY
-gm convert $foto -crop 1000x500+0+0 crop2.png  # width x height + originX + originY
+#gm convert $foto -crop 1000x500+0+0 crop2.png  # width x height + originX + originY
 
 # Valores para recortar la imagen (en pixeles)
 W=1000
@@ -18,4 +18,7 @@ X=0
 Y=200
 input=magick.png
 output=crop.png
-gm convert $input -crop ${W}x${H}+${X}+${Y} $output
+#gm convert $input -crop ${W}x${H}+${X}+${Y} $output
+
+# Recortar en n partes
+gm convert $foto -crop 2x1@ +repage qq-%d.png
